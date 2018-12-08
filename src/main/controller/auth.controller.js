12491f3726a2b61bbb65e88f015d2666aed95ad6
iksrusvn <ikarusvn@gmail.com>
2018-12-08 18:40:58 +0700

    dsdfsda

--- src/main/controller/auth.controller.js
+++ src/main/controller/auth.controller.js
@@ -1,11 +1,13 @@
 const TokenUtils = require('../common/token.utils');
 const Config = require('../config');
+const UserService = require('../service/user.service');
 
 const AuthController = {};
 
-AuthController.login = (req, res) => {
+AuthController.login = async (req, res) => {
     try {
         let loginInfo = req.body;
+        loginInfo.isAdmin = await UserService.isAdmin(loginInfo.userId);
 
         let token = TokenUtils.generateAccessToken(loginInfo);
 
